d084fccab8fd8463d40029124f6210682b3f19f4
LeonardoCG <andresxach7@gmail.com>
2022-02-19 13:36:00 -0600

    prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
-};
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDFT6f11CCYpQjX43_ZJuA9rrX2Yt2e8xU",
+    authDomain: "firechat-40fe7.firebaseapp.com",
+    projectId: "firechat-40fe7",
+    storageBucket: "firechat-40fe7.appspot.com",
+    messagingSenderId: "710538518646",
+    appId: "1:710538518646:web:2e149c1383fec896a97f57",
+    measurementId: "G-MQBQ6KLTYF"
+  }
+}
